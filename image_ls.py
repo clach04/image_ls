7533ab1b9253f51be307c14a8f092a91b01004a7
clach04 <clach04@gmail.com>
2023-01-14 10:54:13 -0800

    Fix issue #4 - refactor, reduce code duplication

--- image_ls.py
+++ image_ls.py
@@ -74,97 +74,48 @@ except AttributeError:
 
 option_accurate_colour_count = False
 
-def doit(dir_name):
+def image_ls(dir_or_archive_name):
+    if os.path.isdir(dir_or_archive_name):
+        is_dir = True
+        is_archive = False
+        dir_name = os.path.abspath(dir_or_archive_name)
+        glob_search_str = os.path.join(glob_escape(dir_name), '*')  # NOTE escape maybe Py3 only? - New in version 3.4.
+        print(repr(dir_name))
+        file_list = glob.glob(glob_search_str)
+        file_list.sort()  # TODO natsort file_list
+    else:
+        # Assume a zip (archive) file
+        is_dir = False
+        is_archive = True
+        zip_filename = os.path.abspath(dir_or_archive_name)
+        print(repr(zip_filename))
+        arch = ZipFile(zip_filename, 'r')
+        file_list = arch.namelist()
+        file_list.sort()  # TODO natsort file_list
+
     counter = 0  # file counter, currently ignores directories
-    dir_name = os.path.abspath(dir_name)
-    glob_search_str = os.path.join(glob_escape(dir_name), '*')  # NOTE escape maybe Py3 only? - New in version 3.4.
-    print(repr(dir_name))
-    file_list = glob.glob(glob_search_str)
-    file_list.sort()  # TODO natsort file_list
     print('%8s %10s %5s %7s %s %s' % ('size', 'res', 'fmt', 'depth', '#col', 'filename'))
+
     for filename in file_list:
-        if os.path.isdir(filename):
-            # skip directories (TODO consider displaying them?)
-            continue
-        counter += 1
-        # TODO grayscale
-        # TODO file timestamp?
-        # filesize, resolution, format - mode/bit-depth - num colors
-        #print('%r' % filename)
-        #print('\t%r' % os.path.basename(filename))
-        """
-        # f is a file-like object.
-        old_file_position = f.tell()
-        f.seek(0, os.SEEK_END)
-        file_size = f.tell()
-        f.seek(old_file_position, os.SEEK_SET)
-        """
-        # os.path.getsize(filename)
-        file_info = os.stat(filename)  # try and retain py2 support, so skip Python3 pathlib
-        try:
-            im = Image.open(filename)
-            if option_accurate_colour_count:
-                # processes all image data (which is expensive)
-                colour_count = len(set(im.getdata()))  # essentially number of colors
-            else:
-                colour_count = pow(2, mode_to_bpp[im.mode])  # max possible (might not all be used)
-            if colour_count >= 1000:
-                colour_count_str = '>999'
-            else:
-                colour_count_str = '%4d' % colour_count
-            format_str = '%4s-%d' % (im.mode, mode_to_bpp[im.mode])
-            image_size_str = '%5dx%d' % im.size  # width, height
-            im_format = im.format
-        except UnidentifiedImageError:
-            # FIXME get lengths correct
-            image_size_str = ''
-            im_format = '???'
-            format_str = ''
-            colour_count_str = '    '
+        if is_dir:
+            if os.path.isdir(filename):
+                # skip directories (TODO consider displaying them?)
+                continue
+            file_info = os.stat(filename)  # try and retain py2 support, so skip Python3 pathlib
+            file_size = file_info.st_size  # os.path.getsize(filename)
+            file_ptr = open(filename, 'rb')
 
-        print('%8s %10s %r %7s %s %r' % (bytesize2human_ls_en(file_info.st_size), image_size_str, im_format, format_str, colour_count_str, os.path.basename(filename)))
-        """
-        print('%r' % im)
-        print('%r' % file_info.st_size)
-        print('%r' % bytesize2human_ls_en(file_info.st_size))
-        print('%r' % (im.size,)) # width, height
-        print('%r' % im.format)
-        print('%r' % im.mode)
-        print('%r' % mode_to_bpp[im.mode])
-        #print('%r' % im.bits)
-        
-        print('%r' % depth)
-        #print('%r' % dir(im.palette))
-        print('%r' % im.format_description)
-        print('%r' % im.get_format_mimetype())
-        print('%r' % im.info)  # look for Orientation - https://pillow.readthedocs.io/en/stable/reference/Image.html#PIL.Image.Image.info
-        """
-    #print('%r' % dir(im))
-    print('%d files' % counter)
-
-
-# FIXME refactor, e.g. doit_zip() image open and processing code duplicates doit()
-def doit_zip(zip_filename):
-    counter = 0  # file counter, currently ignores directories
-    zip_filename = os.path.abspath(zip_filename)
-    print(repr(zip_filename))
-    arch = ZipFile(zip_filename, 'r')
-    file_list = arch.namelist()
-    file_list.sort()  # TODO natsort file_list
-    print('%8s %10s %5s %7s %s %s' % ('size', 'res', 'fmt', 'depth', '#col', 'filename'))
-    for filename in file_list:
-        if filename.endswith('/'):
-            # skip directories (TODO consider displaying them?)
-            continue
-        file_contents = arch.read(filename)  # read into memory
-        file_size = len(file_contents) ## FIXME lookup metadata from arch
-        file_ptr = FakeFile(file_contents)
-        if filename.startswith('/'):
-            # NOTE unix style path
-            # TODO win too
-            filename = filename[1:]
-
-        counter += 1
+        if is_archive:
+            if filename.endswith('/'):
+                # skip directories (TODO consider displaying them?)
+                continue
+            file_contents = arch.read(filename)  # read into memory
+            file_size = len(file_contents) ## FIXME lookup metadata from arch
+            file_ptr = FakeFile(file_contents)
+            if filename.startswith('/'):
+                # NOTE unix style path
+                # TODO win too
+                filename = filename[1:]
 
         # TODO grayscale
         # TODO file timestamp?
@@ -178,7 +129,6 @@ def doit_zip(zip_filename):
         file_size = f.tell()
         f.seek(old_file_position, os.SEEK_SET)
         """
-        # os.path.getsize(filename)
         try:
             im = Image.open(file_ptr)
             if option_accurate_colour_count:
@@ -203,7 +153,28 @@ def doit_zip(zip_filename):
         #filename_basename = os.path.basename(filename)  # for zip file, this will hide paths
         filename_basename = filename
         print('%8s %10s %r %7s %s %r' % (bytesize2human_ls_en(file_size), image_size_str, im_format, format_str, colour_count_str, filename_basename))
-    arch.close()
+        file_ptr.close()
+        counter += 1
+        """
+        print('%r' % im)
+        print('%r' % file_info.st_size)
+        print('%r' % bytesize2human_ls_en(file_info.st_size))
+        print('%r' % (im.size,)) # width, height
+        print('%r' % im.format)
+        print('%r' % im.mode)
+        print('%r' % mode_to_bpp[im.mode])
+        #print('%r' % im.bits)
+        
+        print('%r' % depth)
+        #print('%r' % dir(im.palette))
+        print('%r' % im.format_description)
+        print('%r' % im.get_format_mimetype())
+        print('%r' % im.info)  # look for Orientation - https://pillow.readthedocs.io/en/stable/reference/Image.html#PIL.Image.Image.info
+        """
+        #print('%r' % dir(im))
+
+    if is_archive:
+        arch.close()
     print('%d files' % counter)
 
 
@@ -219,12 +190,7 @@ def main(argv=None):
     except IndexError:
         dir_name = '.'
 
-    # FIXME merge and refactor doit() and doit_zip()
-    if os.path.isdir(dir_name):
-        doit(dir_name)
-    else:
-        # Assume a zip file
-        doit_zip(dir_name)
+    image_ls(dir_name)
 
     return 0
 
